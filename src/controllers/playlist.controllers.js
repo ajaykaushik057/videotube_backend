1c00e14831daf250107cc9b39f57628d96cf231a
ajaykaushik057 <ajaykaushik057@gmail.com>
2024-08-17 22:01:43 +0530

    playlist controller completed

--- src/controllers/playlist.controllers.js
+++ src/controllers/playlist.controllers.js
@@ -1,4 +1,4 @@
-import mongoose, {isValidObjectId} from "mongoose"
+import mongoose, {isValidObjectId, set} from "mongoose"
 import {Playlist} from "../models/playlist.model.js"
 import {Apierror} from "../utils/Apierror.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
@@ -142,12 +142,72 @@ const removeVideoFromPlaylist = asyncHandler(async (req, res) => {
 const deletePlaylist = asyncHandler(async (req, res) => {
     const {playlistId} = req.params
     // TODO: delete playlist
+
+    if (!playlistId.trim() || !isValidObjectId(playlistId)) {
+        throw new Apierror(402,"playlist Id required or Invalid!!")
+    }
+
+    const playlist = await Playlist.findById(playlistId)
+    console.log(playlist);
+    
+
+    if (!playlist) {
+        throw new Apierror(404,"Playlist not found!")
+    }
+
+    if (playlist.owner.toString() != (req.user?._id).toString()) {
+        throw new Apierror(401,"Unauthorized access")
+    }
+
+    const deletedPlaylist = await Playlist.findByIdAndDelete(playlist)
+    console.log(deletedPlaylist);
+    
+
+    return res
+    .status(200)
+    .json(
+        200,
+        new ApiResponse(200,deletedPlaylist,"video playlist deleted successfully! "),
+    )
+
 })
 
 const updatePlaylist = asyncHandler(async (req, res) => {
     const {playlistId} = req.params
     const {name, description} = req.body
     //TODO: update playlist
+
+    if (!playlistId.trim() || !isValidObjectId(playlistId)) {
+        throw new Apierror(400,"Playlist Id required or Invalid ")
+    }
+
+    const playlist = await Playlist.findById(playlistId)
+
+    if (!playlist) {
+        throw new Apierror(404,"playlist not found!")
+    }
+
+    if (playlist.owner.toString() != (req.user?._id).toString()) {
+        throw new Apierror(401,"unauthorized access")
+    }
+
+    if (!(name||description)) {
+        throw new Apierror(400,"name or description required!")
+    }
+
+    const updatedPlaylist = await Playlist.findByIdAndUpdate(
+        playlistId,
+        {$set:{name,description}} ,
+        {new:true}  
+    )
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200,updatedPlaylist,"Updated Successfully!")
+    )
+
+
 })
 
 export {
--- src/routes/playlist.routes.js
+++ src/routes/playlist.routes.js
@@ -18,6 +18,8 @@ router.route("/").post(createPlaylist)
 router.route("/user/:userId").get(getUserPlaylists)
 
 router.route("/:playlistId").get(getPlaylistById)
+                            .delete(deletePlaylist)
+                            .patch(updatePlaylist)
 
 router.route("/add/:videoId/:playlistId").patch(addVideoToPlaylist)
 
